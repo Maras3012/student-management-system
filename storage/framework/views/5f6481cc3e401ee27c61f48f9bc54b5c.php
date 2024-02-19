276257c4e258c3ad0b3fd04ba2a7b24aee543a35
Maras3012 <60932373+Maras3012@users.noreply.github.com>
2024-02-19 23:05:16 +0100

    Add files via upload

---
+++ storage/framework/views/5f6481cc3e401ee27c61f48f9bc54b5c.php
@@ -0,0 +1,17 @@
+<?php $__env->startSection('content'); ?>
+
+
+    <div class="card">
+        <div class="card-header">Batches Page</div>
+        <div class="card-body">
+            <div class="card-body">
+                <h5 class="card-title">Name : <?php echo e($batches->name); ?></h5>
+                <p class="card-text">Course : <?php echo e($batches->course->name); ?></p>
+                <p class="card-text">Start Date : <?php echo e($batches->start_date); ?></p>
+            </div>
+            </hr>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\wamp64\www\student-management-system\resources\views/batches/show.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/60444ed3aa77addd657a5884143415c0.php
@@ -0,0 +1,26 @@
+
+<?php $__env->startSection('content'); ?>
+    <div class="card">
+        <div class="card-header">Batches Page</div>
+        <div class="card-body">
+
+            <form action="<?php echo e(url('batches')); ?>" method="post">
+                <?php echo csrf_field(); ?>
+
+                <label>Name</label></br>
+                <input type="text" name="name" id="name" class="form-control"></br>
+                <label>Course</label></br>
+                <select name="course_id" id="course_id" class="form-control">
+                    <?php $__currentLoopData = $courses; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $id => $name): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <option value="<?php echo e($id); ?>"><?php echo e($name); ?></option>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </select>
+                <label>Start Date</label></br>
+                <input type="text" name="start_date" id="start_date" class="form-control"></br>
+                <input type="submit" value="Save" class="btn btn-success"></br>
+            </form>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\wamp64\www\student-management-system\resources\views/batches/create.blade.php ENDPATH**/ ?>
